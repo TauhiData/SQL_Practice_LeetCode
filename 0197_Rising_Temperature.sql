499786b8a38507d41ad1100a6955b2ebdc672de0
Tauhid Choudhury <taucho0.07@gmail.com>
2026-03-17 02:04:35 +0000

    Update 0197_Rising_Temperature.sql

--- 0197_Rising_Temperature.sql
+++ 0197_Rising_Temperature.sql
@@ -13,10 +13,10 @@ There are no different rows with the same recordDate.
 This table contains information about the temperature on a certain day.
  
 Write a solution to find all dates 'id' with higher temperatures compared to its previous dates (yesterday
-  
+
 Answer:
- SELECT W1.id 
- FROM Weather W1
- JOIN Weather W2
- ON W1.recordDate = DATE_ADD(W2.recordDate, INTERVAL 1 DAY)
+SELECT W1.id 
+FROM Weather W1
+JOIN Weather W2
+ON W1.recordDate = DATE_ADD(W2.recordDate, INTERVAL 1 DAY)
 WHERE W1.temperature > W2.temperature;
