7354c769e7bf3e6409a83af5a757709d6da345ae
Tauhid Choudhury <taucho0.07@gmail.com>
2026-05-05 01:40:29 +0100

    Create 2356_Number_of_Unique_Subjects_Taught_by_Each_Teacher.sql

---
+++ 2356_Number_of_Unique_Subjects_Taught_by_Each_Teacher.sql
@@ -0,0 +1,22 @@
+Question:
+ Table: Teacher
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| teacher_id  | int  |
+| subject_id  | int  |
+| dept_id     | int  |
++-------------+------+
+(subject_id, dept_id) is the primary key (combinations of columns with unique values) of this table.
+Each row in this table indicates that the teacher with teacher_id teaches the subject subject_id in the department dept_id.
+Write a solution to calculate the number of unique subjects each teacher teaches in the university.
+Return the result table in any order.
+
+Answer:
+ SELECT 
+ t.teacher_id ,
+ COUNT(DISTINCT t.subject_id) AS cnt
+FROM Teacher t
+GROUP BY t.teacher_id;
+
+ 
