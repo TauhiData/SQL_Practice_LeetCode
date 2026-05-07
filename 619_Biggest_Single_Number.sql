0b5c72774eeb28f42e668ef82a4e86aaf4d45447
Tauhid Choudhury <taucho0.07@gmail.com>
2026-05-07 10:02:56 +0100

    Create 619_Biggest_Single_Number.sql

---
+++ 619_Biggest_Single_Number.sql
@@ -0,0 +1,24 @@
+Question:
+ Table: MyNumbers
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| num         | int  |
++-------------+------+
+This table may contain duplicates (In other words, there is no primary key for this table in SQL).
+Each row of this table contains an integer.
+ 
+
+A single number is a number that appeared only once in the MyNumbers table.
+
+Find the largest single number. If there is no single number, report null.
+
+Answer:
+ SELECT MAX(num) as num
+FROM mynumbers n
+where num IN (
+    SELECT num
+    FROM mynumbers
+    GROUP BY num
+    HAVING COUNT(*)=1 );
